b0f02170284b76d5babc3d7ca9b3783c99e07c62
m.esmaeili <esmaeily.mhe@isi.com>
2026-07-24 13:43:15 +0330

    add AcademicYear

--- owlet-api/src/main/java/com/owlet/api/dto/org/AcademicYearCreateRequest.java
+++ owlet-api/src/main/java/com/owlet/api/dto/org/AcademicYearCreateRequest.java
@@ -1,13 +1,11 @@
 package com.owlet.api.dto.org;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.owlet.api.dto.BaseDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 @Getter
 @Setter
