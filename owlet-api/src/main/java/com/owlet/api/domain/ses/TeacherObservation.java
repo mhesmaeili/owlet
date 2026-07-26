7ec8c32c4b2e16f624e709b6d862f000d07927ed
m.esmaeili <esmaeily.mhe@isi.com>
2026-07-26 22:37:39 +0330

    refactor

--- owlet-api/src/main/java/com/owlet/api/domain/ses/TeacherObservation.java
+++ owlet-api/src/main/java/com/owlet/api/domain/ses/TeacherObservation.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
 
 @Getter
 @Setter
--- owlet-api/src/main/java/com/owlet/api/dto/ses/SessionStudentDto.java
+++ owlet-api/src/main/java/com/owlet/api/dto/ses/SessionStudentDto.java
@@ -1,11 +1,7 @@
 package com.owlet.api.dto.ses;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.owlet.api.domain.ses.Session;
-import com.owlet.api.domain.ses.SessionStudent;
 import com.owlet.api.dto.BaseDto;
-import com.owlet.api.dto.idm.AccountDto;
-import com.owlet.api.dto.std.StudentDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
