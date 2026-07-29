efecc5d96c4ca9b1e60aaf74af9ec17931614306
m.esmaeili <esmaeily.mhe@isi.com>
2026-07-29 22:14:53 +0330

    add exception

---
+++ owlet-common/src/main/java/com/owlet/common/exception/StorageException.java
@@ -0,0 +1,9 @@
+package com.owlet.common.exception;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
