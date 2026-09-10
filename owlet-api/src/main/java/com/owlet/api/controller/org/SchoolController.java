291940cd0cb4cab01bc0c7e335bcbff6605cce1d
m.esmaeili <esmaeily.mhe@isi.com>
2026-09-10 13:14:51 +0330

    add for load school in homepage

--- owlet-api/src/main/java/com/owlet/api/controller/org/SchoolController.java
+++ owlet-api/src/main/java/com/owlet/api/controller/org/SchoolController.java
@@ -1,11 +1,17 @@
 package com.owlet.api.controller.org;
 
+import com.owlet.api.annotation.PublicEndpoint;
 import com.owlet.api.controller.base.CrudController;
+import com.owlet.api.dto.edu.ProductDto;
 import com.owlet.api.dto.org.SchoolCreateRequest;
 import com.owlet.api.dto.org.SchoolDto;
 import com.owlet.api.dto.org.SchoolUpdateRequest;
 import com.owlet.api.service.org.SchoolService;
+import com.owlet.common.response.ApiResponse;
+import com.owlet.common.response.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +28,14 @@ public class SchoolController extends CrudController<
 
     public SchoolController(SchoolService service) {
         super(service);
+    }
+
+
+    @PublicEndpoint
+    @Override
+    @GetMapping("/search")
+    public ApiResponse<PageResponse<SchoolDto>> search(String keyword, Pageable pageable) {
+        return super.search(keyword, pageable);
     }
 
 }
